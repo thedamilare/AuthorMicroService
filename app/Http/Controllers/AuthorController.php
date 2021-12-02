f3092ec16bce84337cff726760d475299e6d59b8
Samuel Adegunle <sadegunle@softalliance.com>
2021-12-02 13:59:31 +0100

    author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return list of authors
+     * 
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create a new author
+     * 
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Show an existing author
+     * 
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+
+    }
+
+    /**
+     * Update an existing author
+     * 
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    /**
+     * Delete an existing author
+     * 
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+
+    }
+}
